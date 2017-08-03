7abadcebbf078dbf9f2f58111bd45161a0d764a7
Duan <qydyx2009@163.com>
2017-08-03 09:38:44 +0800

    Try to push these to the remote repository.

---
+++ src/common.h
@@ -0,0 +1,24 @@
+#ifndef GFS_COMMON_H
+#define GFS_COMMON_H
+
+#include <msgpack.hpp>
+
+typedef std::uint64_t ChunkHandle;
+typedef std::uint64_t ChunkVersion;
+
+enum class GFSErrorCode : std::uint32_t
+{
+	OK = 0,
+};
+
+struct GFSError
+{
+	GFSErrorCode errCode;
+	std::string description;
+
+	MSGPACK_DEFINE(errCode, description);
+};
+
+MSGPACK_ADD_ENUM(GFSErrorCode);
+
+#endif
